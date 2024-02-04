bd894e1cec5bbe1d39045853a179dd0a107a21f9
wmjchf <3284304056@qq.com>
2024-02-04 19:48:17 +0800

    feat: 增加react依赖

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,9 @@
-export const getName = () => {
-  return "dogwang";
-};
+import React from "react";
+
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const root = createRoot(document.querySelector("#app"));
+
+root.render(<App></App>);

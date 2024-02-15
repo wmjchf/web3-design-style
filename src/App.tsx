3ff8113ad8e06811e3bac08b2ea6a7970e34addb
wmjchf <3284304056@qq.com>
2024-02-15 21:35:48 +0800

    feat: 删除wagmi v2.0相关代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,5 @@
 import React from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { WagmiProvider } from "wagmi";
-import { config } from "./wagmi/config";
-import { ConnectWallet } from "./components/ConnectWallet";
-
-const queryClient = new QueryClient();
 
 export const App = () => {
-  return (
-    <WagmiProvider config={config}>
-      <QueryClientProvider client={queryClient}>
-        <ConnectWallet></ConnectWallet>
-      </QueryClientProvider>
-    </WagmiProvider>
-  );
+  return <></>;
 };
--- src/wagmi/config.ts
+++
@@ -1,9 +0,0 @@
-import { http, createConfig } from "wagmi";
-import { localhost } from "./definedChain";
-
-export const config = createConfig({
-  chains: [localhost],
-  transports: {
-    [localhost.id]: http(),
-  },
-});
